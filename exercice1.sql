db0bda10f18bf37cd96c5ef8f18694f16ca57634
HichemChicha <hichemchicha2@gmail.com>
2025-05-19 16:07:38 +0000

    exercice1.sql

--- exercice1.sql
+++ exercice1.sql
@@ -9,3 +9,26 @@ INSERT INTO Materiel (Id_Materiel, nom_materiel, Quantite) VALUES
 ('M008', 'Microphone Blue Yeti', '4'),
 ('M009', 'Casque audio', '3'),
 ('M010', 'Imprimante 3D', '1');
+
+INSERT INTO Reservations (Id_Reservation, date_début, date_fin, Id_Utilisateurs, Id_Materiel) VALUES
+(1, '2025-05-01', '2025-05-05', 1, 'M002'),
+(2, '2025-05-03', '2025-05-04', 2, 'M001'),
+(3, '2025-05-06', '2025-05-10', 3, 'M003'),
+(4, '2025-05-04', '2025-05-06', 4, 'M004'),
+(5, '2025-05-02', '2025-05-07', 5, 'M005');
+
+INSERT INTO Utilisateurs (Id_Utilisateurs, Nom, Prenom, email) VALUES
+(1, 'Durand', 'Alice', 'alice.durand@example.com'),
+(2, 'Martin', 'Bob', 'bob.martin@example.com'),
+(3, 'Lemoine', 'Carla', 'carla.lemoine@example.com'),
+(4, 'Nguyen', 'David', 'david.nguyen@example.com'),
+(5, 'Petit', 'Eva', 'eva.petit@example.com'),
+(6, 'Dupont', 'Franck', 'franck.dupont@example.com'),
+(7, 'Moreau', 'Gina', 'gina.moreau@example.com'),
+(8, 'Garcia', 'Hugo', 'hugo.garcia@example.com'),
+(9, 'Roux', 'Ismaël', 'ismael.roux@example.com'),
+(10, 'Leclerc', 'Julie', 'julie.leclerc@example.com');
+
+SELECT * FROM Utilisateurs;
+SELECT * FROM Materiel;
+SELECT * FROM Reservations;
