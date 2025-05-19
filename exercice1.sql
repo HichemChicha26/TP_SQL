6d7e6d60d868567c5de0c30de95bd566e00c7613
HichemChicha <hichemchicha2@gmail.com>
2025-05-19 15:48:14 +0000

    import

---
+++ exercice1.sql
@@ -0,0 +1,11 @@
+INSERT INTO Materiel (Id_Materiel, nom_materiel, Quantite) VALUES
+('M001', 'Projecteur HD', '3'),
+('M002', 'Ordinateur portable', '5'),
+('M003', 'Tablette iPad', '4'),
+('M004', 'Câble HDMI', '10'),
+('M005', 'Souris sans fil', '8'),
+('M006', 'Clavier mécanique', '6'),
+('M007', 'Appareil photo', '2'),
+('M008', 'Microphone Blue Yeti', '4'),
+('M009', 'Casque audio', '3'),
+('M010', 'Imprimante 3D', '1');
